cecf5f6aec1ac400712c04bae243278a22899c80
Ashish7368 <153342595+Ashish7368@users.noreply.github.com>
2025-07-17 22:33:05 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:7319979768"><i class="fas fa-phone"></i> +917319979768</a>
+         <a href="tel:8507821157"><i class="fas fa-phone"></i> +918507821157</a>
+         <a href="mailto:avashish1012@gmail.com"><i class="fas fa-envelope"></i>avashish1012@gmail.com</a>
+         <a href="https://https://maps.app.goo.gl/AENZuPpdZxNPSSNP6?g_st=aw"><i class="fas fa-map-marker-alt"></i> Hajipur, Bihar, India - 844101</a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://facebook.com/Ashish.Verma.7?mibextid=ZbWKwL"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://facebook.com/Ashish.verma.7?mibextid=ZbWKwL"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="https://instagram.com/im.Ashish.verma?igshid=OGRjNzg3M2Y="><i class="fab fa-instagram"></i>instagram</a>
+         <a href="https://www.linkedin.com/in/ashish-verma-70a637283?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Ashish Verma</span> | All rights reserved!</div>
+
+</footer>
